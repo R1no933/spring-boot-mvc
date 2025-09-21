03617d60e57ce84f6a931f53294ac8aed091cf1a
r1no933 <dbaskakov93@yandex.ru>
2025-09-21 16:02:57 +0300

    init project

---
+++ src/main/java/baskakov/dev/springboormvc/SpringBoorMvcApplication.java
@@ -0,0 +1,13 @@
+package baskakov.dev.springboormvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoorMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoorMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/baskakov/dev/springboormvc/SpringBoorMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package baskakov.dev.springboormvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoorMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
