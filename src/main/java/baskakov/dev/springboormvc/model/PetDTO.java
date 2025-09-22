38ac4cfce9a5dc01c2fa18764f782fe09f24e578
r1no933 <dbaskakov93@yandex.ru>
2025-09-22 15:29:15 +0300

    добавил валидацию для полей DTO

--- src/main/java/baskakov/dev/springboormvc/model/PetDTO.java
+++ src/main/java/baskakov/dev/springboormvc/model/PetDTO.java
@@ -1,8 +1,18 @@
 package baskakov.dev.springboormvc.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Size;
+
 public class PetDTO {
+    @Null
     private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 30)
     private String name;
+
+    @Null
     private Long userId;
 
     public PetDTO(Long id, String name, Long userId) {
